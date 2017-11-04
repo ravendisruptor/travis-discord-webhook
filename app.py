61682b49e53ece62ebcd2927d1dbe7d94f9b85d7
CreepPork <creepporkstudios@gmail.com>
2017-11-05 00:51:05 +0200

    aaaand now?

--- app.py
+++ app.py
@@ -29,8 +29,7 @@ def badge():
     data = requests.get(DISCORD_JSON)
     data = json.loads(data.text)
     numberOfOnlineUsers = len(data['members'])
-    onlineString = " Online"
-    onlineUsersString = numberOfOnlineUsers + onlineString
+    onlineUsersString = (str numberOfOnlineUsers) + " Online"
 
     return json.dumps({'users': onlineUsersString})
 
