95a5aa4d60350faed4f5c64dd14b9a61b193d034
CreepPork <creepporkstudios@gmail.com>
2018-04-07 13:34:12 +0300

    Added Karmanor support

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ with open("config.yaml") as file:
     config = yaml.load(file)
 
 DISCORD_WEBHOOK = config["discord-webhook"]
+DISCORD_WEBHOOK_KARMANOR = config["discord-webhook-karmanor"]
+
 DISCORD_JSON = config["discord-json"]
 COLORS = config["colors"]
 
@@ -27,21 +29,25 @@ app = Flask(__name__)
 # Is this even needed?
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "idk")
 
+
 @app.route("/badge", methods=["GET"])
 def badge():
     # Gets online users
     data = requests.get(DISCORD_JSON)
     data = json.loads(data.text)
-    onlineUsersString = str (len(data['members'])) + " Online"
+    onlineUsersString = str(len(data['members'])) + " Online"
 
     # Gets total downloaded (from SW and GitHub)
     # Gets lifetime subscriptions from Steam Workshop
-    data = requests.post(SW_JSON, {"itemcount": 1, "publishedfileids[0]": SW_JSON_ID})
+    data = requests.post(
+        SW_JSON, {"itemcount": 1, "publishedfileids[0]": SW_JSON_ID})
     data = json.loads(data.text)
-    totalDownloadsSteam = int(data["response"]["publishedfiledetails"][0]["lifetime_subscriptions"])
+    totalDownloadsSteam = int(
+        data["response"]["publishedfiledetails"][0]["lifetime_subscriptions"])
 
     # Gets all downloads from GitHub
-    data = requests.get('https://api.github.com/repos/ArmaAchilles/AresModAchillesExpansion/releases')
+    data = requests.get(
+        'https://api.github.com/repos/ArmaAchilles/AresModAchillesExpansion/releases')
     data = json.loads(data.text)
     totalDownloadsGitHub = 0
 
@@ -59,11 +65,13 @@ def badge():
         {
             'users': onlineUsersString,
             'downloads': human_format(totalDownloadsSteam + totalDownloadsGitHub),
-            'version': tagName.replace("v","")
+            'version': tagName.replace("v", "")
         }
     ), mimetype='application/json')
 
 # https://stackoverflow.com/a/579376
+
+
 def human_format(num):
     magnitude = 0
     while abs(num) >= 1000:
@@ -71,6 +79,7 @@ def human_format(num):
         num /= 1000.0
     # add more suffixes if you need them
     return '{}{}'.format(round(num), ['', 'k', 'm'][magnitude])
+
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
@@ -108,7 +117,51 @@ def webhook():
         }]
     }
 
-    resp = requests.request("POST", DISCORD_WEBHOOK, json=payload, headers={"Content-Type": "application/json"})
+    resp = requests.request("POST", DISCORD_WEBHOOK, json=payload, headers={
+                            "Content-Type": "application/json"})
+
+    # https://stackoverflow.com/a/19569090
+    return resp.text, resp.status_code, resp.headers.items()
+
+
+@app.route("/webhook_karmanor", methods=["POST"])
+def webhook_karmanor():
+    data = request.form["payload"]
+    data = json.loads(data)
+
+    if (data["repository"]["owner_name"] != "ArmaAchilles"):
+        sys.exit()
+
+    # Force lower because yaml uses lower case
+    result = data["status_message"].lower()
+
+    color = COLORS[result]
+
+    time = "started_at" if result == "pending" else "finished_at"
+
+    # PHP example just uses array() but that doesn't make sense...
+    # Idk, should ask someone who PHPs
+    payload = {
+        "username": "Travis CI",
+        "avatar_url": "https://i.imgur.com/kOfUGNS.png",
+        "embeds": [{
+            "color": color,
+            "author": {
+                "name": data["author_name"]
+                # TODO: See if author username can be found in
+                # Travis' payload, and then
+                # `"icon_url" : "https://github.com/USERNAME.png`
+                # as described in https://stackoverflow.com/a/36380674
+            },
+            "title": PAYLOAD_TITLE.format(**data, result_text=result.capitalize()),
+            "url": data["build_url"],
+            "description": PAYLOAD_DESCRIPTION.format(**data, url=PAYLOAD_COMMIT_URL.format(**data)),
+            "timestamp": data[time]
+        }]
+    }
+
+    resp = requests.request("POST", DISCORD_WEBHOOK_KARMANOR, json=payload, headers={
+                            "Content-Type": "application/json"})
 
     # https://stackoverflow.com/a/19569090
     return resp.text, resp.status_code, resp.headers.items()
